e040b379fbc7b17702663ccd2f7989db43ae01d8
uncledaddy13 <143741338+uncledaddy13@users.noreply.github.com>
2023-08-31 12:31:37 -0500

    Update streamlit_app.py
    Added Emoji, Avocado Toast and New Header for BYOFS

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,16 @@ import streamlit;
 
 streamlit.title('My new parents healthy diner')
 
-
+   
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boild Egg Free-Range Egg')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boild Egg Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
